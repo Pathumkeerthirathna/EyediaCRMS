1cc24266fd666ba5b84da6b36bb3943161de0e64
pathu <pathu@DESKTOP-ITS8CAK>
2025-08-17 22:47:42 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+console.log(">>> VITE CONFIG LOADED <<<");
+
 // https://vite.dev/config/
 export default defineConfig({
+
+  
   plugins: [
     react(),
     tailwindcss(),
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://eyediacrmsapi.runasp.net", // your backend
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/api"), 
+      },
+    },
+  },
 })
